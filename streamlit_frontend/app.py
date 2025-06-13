56d32424ec8ea4a55e2c198beda22a594015dc9a
T. Kawai <28555632+kawaitaAGU@users.noreply.github.com>
2025-06-13 11:18:38 +0900

    Update app.py

--- streamlit_frontend/app.py
+++ streamlit_frontend/app.py
@@ -27,7 +27,7 @@ if st.button("送信してMCPツールを呼び出す"):
                 {
                     "type": "openapi",
                     "spec": {
-                        "url": MCP_OPENAPI_URL
+                        "url": https://mcp-test01.onrender.com
                     }
                 }
             ]
